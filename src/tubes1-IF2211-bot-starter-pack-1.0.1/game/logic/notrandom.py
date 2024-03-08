3e93311f7729f6a421da3edf6e84aee798cc3933
LastPrism7 <67941358+LastPrism7@users.noreply.github.com>
2024-03-08 08:24:32 +0700

    fix: 5th diamond red and teleporter values

--- src/tubes1-IF2211-bot-starter-pack-1.0.1/game/logic/notrandom.py
+++ src/tubes1-IF2211-bot-starter-pack-1.0.1/game/logic/notrandom.py
@@ -1,15 +1,12 @@
-import random
 from typing import Optional
-
 from game.logic.base import BaseLogic
 from game.models import GameObject, Board, Position
 from ..util import get_direction
 
-#issues:
-#red diamond point difference not accounted for somehow
-#stuck on red diamond if carrying four diamonds
+#unsolved
+#teleporter in path to home unnacounted for
 
-class NotrandomLogic(BaseLogic):
+class RandomLogic(BaseLogic):
     def __init__(self):
         self.directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
         self.goal_position: Optional[Position] = None
@@ -28,11 +25,11 @@ class NotrandomLogic(BaseLogic):
         for i in range (len(diamond_list)):
             distance_list[i] = abs(current_pos_x - diamond_list[i].position.x) + abs(diamond_list[i].position.y - current_pos_y)
             if diamond_list[i].properties.points == 2:
-                distance_list[i]*0.3
+                distance_list[i]*=0.6
         distance_pos_list = list(zip(distance_list,diamond_list))
         distance_pos_list.sort(key=lambda x: x[0])
-        print(distance_pos_list[0][1].position)
-        print(props.base)
+        #print(distance_pos_list[0][1].position)
+        #print(props.base)
 
 
 
@@ -51,21 +48,22 @@ class NotrandomLogic(BaseLogic):
             close_tp_dist = dist_tp2
             far_tp = tp_1
             far_tp_dist = dist_tp1
-
+        
         distance_tp_list = [0 for i in range (len(diamond_list))]
         for i in range (len(diamond_list)):
-            distance_tp_list[i] = abs(far_tp.position.x - diamond_list[i].position.x) + abs(far_tp.position.y - diamond_list[i].position.y)
+            distance_tp_list[i] = abs(far_tp.position.x - diamond_list[i].position.x) + abs(far_tp.position.y - diamond_list[i].position.y) + close_tp_dist
             if diamond_list[i].properties.points == 2:
-                distance_tp_list[i]*0.6
+                distance_tp_list[i]*=0.6
         distance_pos_tp_list = list(zip(distance_tp_list,diamond_list))
         distance_pos_tp_list.sort(key=lambda x: x[0])
 
         buton_list = [b for b in board.game_objects if b.type == "DiamondButtonGameObject"]
         buton = buton_list[0]
-        dist_but = abs(buton.position.x - current_pos_x) + abs(buton.position.y - current_pos_y)
+        dist_but = (abs(buton.position.x - current_pos_x) + abs(buton.position.y - current_pos_y))*1.8 + 1
 
 
-        print(distance_pos_list[0][1])
+        #print(distance_pos_tp_list[0][1].properties.points)
+       
         # Analyze new state
         if props.diamonds == 5 or (abs(current_pos_x - board_bot.properties.base.x) + abs(board_bot.properties.base.y - current_pos_y) + 1)>=(props.milliseconds_left / 1000):
             # Move to base
@@ -73,28 +71,33 @@ class NotrandomLogic(BaseLogic):
             self.goal_position = base
             
         else:
-            if props == 4 and distance_pos_list[0][1].properties.points == 2:
+            if props.diamonds == 4 and distance_pos_list[0][1].properties.points == 2:
                 i=1
-                while i<len(diamond_list):
-                    closest = min(dist_but*1.8, distance_pos_tp_list[i][0]+close_tp_dist, distance_pos_list[i][0])
+                while i<len(diamond_list)-1:
+                    closest = min(dist_but*1.8, distance_pos_tp_list[i][0], distance_pos_list[i][0])
                     if closest == distance_pos_list[i][0] and distance_pos_list[i][1].properties.points==1:
                         self.goal_position = distance_pos_list[i][1].position
                         break
-                    elif closest == distance_pos_tp_list[i][0]+close_tp_dist and distance_pos_tp_list[i][1].properties.points==1:
+                    elif closest == distance_pos_tp_list[i][0] and distance_pos_tp_list[i][1].properties.points==1:
                         self.goal_position = close_tp.position
                         break
-                    else:
+                    elif distance_pos_tp_list[i][0] > dist_but and distance_pos_list[i][0] > dist_but:
                         self.goal_position = buton.position
                         break
-                if distance_pos_list[i][1].properties.points==1:
+                    else:
+                        i+=1
+                    
+                if distance_pos_list[i][1].properties.points==2:
                     base = board_bot.properties.base
                     self.goal_position = base
+                else:
+                    self.goal_position = distance_pos_list[i][1].position
                     
             else:
-                closest = min(dist_but, distance_pos_tp_list[0][0]+close_tp_dist, distance_pos_list[0][0])
+                closest = min(dist_but, distance_pos_tp_list[0][0], distance_pos_list[0][0])
                 if closest == distance_pos_list[0][0]:
                     self.goal_position = distance_pos_list[0][1].position
-                elif closest == distance_pos_tp_list[0][0]+close_tp_dist:
+                elif closest == distance_pos_tp_list[0][0]:
                     self.goal_position = close_tp.position
                 else:
                     self.goal_position = buton.position
@@ -110,4 +113,3 @@ class NotrandomLogic(BaseLogic):
             )
         
         return delta_x, delta_y
-
